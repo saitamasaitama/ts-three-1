d2a92f6bd35af5fd1953d02f220445c6738f10ab
Ubuntu <ubuntu@ip-172-26-8-119.ap-northeast-1.compute.internal>
2022-11-14 22:46:28 +0000

    work

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import * as THREE from "three";
+
+// シーンの準備
+const scene = new THREE.Scene();
+
+// カメラの準備
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+);
+
+//         // レンダラーの準備
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//         // ライトの準備
+const directionalLight = new THREE.DirectionalLight("#ffffff", 1);
+directionalLight.position.set(0, 10, 10);
+scene.add(directionalLight);
+//
+//         // 立方体の準備
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+const box = new THREE.Mesh(geometry, material);
+box.position.z = -5;
+scene.add(box);
+
+//         // アニメーションループの開始
+function animate() {
+	requestAnimationFrame(animate);
+	box.rotation.x += 0.01;
+	box.rotation.y += 0.01;
+	renderer.render(scene, camera);
+}
+animate();
+
